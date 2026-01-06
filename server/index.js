6f7a978c0eb5cc6a0382b37ddc4ce6b55ea6fb9b
PranoyC1 <kuttuspranoy23@gmail.com>
2026-01-07 01:34:13 +0530

    Update CORS configuration to allow multiple origins

--- server/index.js
+++ server/index.js
@@ -25,8 +25,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-        origin: "http://localhost:3000",
-        credentials: true,
+    origin: [
+      "http://localhost:3000",
+      "https://studynotion-an-edtech-platform-qgo4.onrender.com",
+    ],
+    credentials: true,
   })
 );
 app.use(
